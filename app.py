fd339955ca038a10ad3eba50382f8aa8fb4097a2
Voodoo33000 <rudloff.martin@googlemail.com>
2024-04-17 00:07:48 +0200

    fancy stuff

--- app.py
+++ app.py
@@ -2,19 +2,49 @@ import sys
 from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
-import requests
 from influxdb_client import InfluxDBClient
 from PyQt5.QtCore import Qt, QThread, QTimer, pyqtSignal
-from PyQt5.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
-                             QLabel, QLCDNumber, QProgressBar, QPushButton,
-                             QSizePolicy, QSpacerItem, QStackedWidget,
-                             QVBoxLayout, QWidget, QGroupBox)
+from PyQt5.QtWidgets import (
+    QApplication,
+    QFrame,
+    QGridLayout,
+    QHBoxLayout,
+    QLabel,
+    QLCDNumber,
+    QProgressBar,
+    QPushButton,
+    QSizePolicy,
+    QSpacerItem,
+    QStackedWidget,
+    QVBoxLayout,
+    QWidget,
+    QGroupBox,
+)
+import qdarkstyle
 from PyQt5.QtGui import QFont
 
 from dotenv import load_dotenv
 import os
 
+glow_style = """
+QLabel {
+    color: #f8f8f2;
+    background-color: #44475a;
+    border: 2px solid #6272a4;
+    padding: 5px;
+    border-radius: 10px;
+    text-shadow: 0 0 10px rgba(255, 255, 255, 0.7);
+}
+"""
 
+def load_stylesheet(file_path):
+    """Lädt ein Stylesheet aus einer Datei in einen String."""
+    try:
+        with open(file_path, "r") as file:
+            return file.read()
+    except IOError:
+        print("Stylesheet-Datei konnte nicht geladen werden.")
+        return ""
 
 
 class DataThread(QThread):
@@ -26,10 +56,15 @@ class DataThread(QThread):
         self.url = url
 
     def run(self):
-        client = InfluxDBClient(url=os.environ.get("influx_url"),
-                                token=os.environ.get("influx_token"), org=os.environ.get("influx_org"))
+        client = InfluxDBClient(
+            url=os.environ.get("influx_url"),
+            token=os.environ.get("influx_token"),
+            org=os.environ.get("influx_org"),
+        )
         query_api = client.query_api()
-        query = '''
+        query = """
+import "date"
+
 dataWattage = from(bucket: "Strom")
   |> range(start: -24h)
   |> filter(fn: (r) => r["_measurement"] == "vz_measurement")
@@ -38,14 +73,19 @@ dataWattage = from(bucket: "Strom")
   |> group(columns: ["uuid"])
 
 dataCounter = from(bucket: "Strom")
-  |> range(start: -1h)
+  |> range(start: date.truncate(t: now(), unit: 1d), stop: now())
   |> filter(fn: (r) => r["_measurement"] == "vz_measurement")
   |> filter(fn: (r) => r["_field"] == "value")
   |> filter(fn: (r) => r["uuid"] == "22792059-416a-4117-8b3a-420e34a841a1")
   |> group(columns: ["uuid"])
+
+latestCounter = dataCounter
   |> last()
   |> set(key: "_field", value: "currentCounter")
 
+startCounter = dataCounter
+  |> first()
+  |> set(key: "_field", value: "startofdayCounter")
 
 minValue = dataWattage
   |> min()
@@ -64,21 +104,23 @@ latestValue = dataWattage
   |> set(key: "_field", value: "latestValue")
 
 // Kombinieren von Min und Max
-union(tables: [minValue, maxValue, avgValue, latestValue, dataCounter])
-'''
+union(tables: [minValue, maxValue, avgValue, latestValue, latestCounter, startCounter]) 
+"""
         result = query_api.query(query=query)
         data_dict = {}
 
         for table in result:
             for record in table.records:
                 # Verwende _field als Schlüssel im Dictionary
-                field_key = record.get_field()  # Dies könnte 'minValue', 'maxValue' oder 'currentCounter' sein, je nachdem wie Sie 'set' in Ihrer Flux-Abfrage verwenden
+                field_key = (
+                    record.get_field()
+                )  # Dies könnte 'minValue', 'maxValue' oder 'currentCounter' sein, je nachdem wie Sie 'set' in Ihrer Flux-Abfrage verwenden
                 if field_key:
                     data_dict[field_key] = {
-                        '_time': record.get_time(),
-                        '_value': record.get_value(),
-                        '_measurement': record.get_measurement(),
-                        'uuid': record.values.get('uuid')
+                        "_time": record.get_time(),
+                        "_value": record.get_value(),
+                        "_measurement": record.get_measurement(),
+                        "uuid": record.values.get("uuid"),
                     }
 
         client.close()
@@ -96,23 +138,31 @@ class MyApp(QWidget):
 
     def initUI(self):
         self.setGeometry(100, 100, 800, 400)
-        self.setWindowTitle('Pimmelzähler Info Display')
+        self.setWindowTitle("Pimmelzähler Info Display")
 
         # Zählerstand
         self.lcd_zaehlerstand = QLCDNumber(self)
         self.lcd_zaehlerstand.setSizePolicy(
-            QSizePolicy.Expanding, QSizePolicy.Expanding)
+            QSizePolicy.Expanding, QSizePolicy.Expanding
+        )
         # Anzahl der Ziffern, die angezeigt werden können
         self.lcd_zaehlerstand.setDigitCount(10)
         self.lcd_zaehlerstand.display(888888888)  # Beispielwert
 
+        self.lcd_kulm = QLCDNumber(self)
+        self.lcd_kulm.setSizePolicy(
+            QSizePolicy.Expanding, QSizePolicy.Expanding
+        )
+        # Anzahl der Ziffern, die angezeigt werden können
+        self.lcd_kulm.setDigitCount(10)
+        self.lcd_kulm.display(0)  # Beispielwert
+
         self.lcd_current = QLCDNumber(self)
-        self.lcd_current.setSizePolicy(
-            QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.lcd_current.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         # Anzahl der Ziffern, die angezeigt werden können
         self.lcd_current.setDigitCount(5)
         self.lcd_current.display(88888)  # Beispielwert
-        self.ts_label_current = QLabel('Warten auf Daten')
+        self.ts_label_current = QLabel("Warten auf Daten")
 
         # Hauptlayout
         layout = QHBoxLayout(self)
@@ -124,95 +174,105 @@ class MyApp(QWidget):
         # Stacked Widget
         self.stackedWidget = QStackedWidget(self)
 
-        content_layout1 = self.create_page('Zählerstand')
-        content_layout1.addWidget(QLabel('kWh'), alignment=Qt.AlignRight)
+        content_layout1 = self.create_page("Zählerstand")
+        content_layout1.addWidget(QLabel("kWh"), alignment=Qt.AlignRight)
         content_layout1.addWidget(self.lcd_zaehlerstand)
-        content_layout1.addWidget(QLabel('Letzte Aktualisierung'))
+        content_layout1.addWidget(QLabel("Letzte Aktualisierung"))
 
-        content_layout2 = self.create_page('Leistunfsaufnahme')
-        content_layout2.addWidget(QLabel('W'), alignment=Qt.AlignRight)
+        content_layout2 = self.create_page("Leistunfsaufnahme")
+        content_layout2.addWidget(QLabel("W"), alignment=Qt.AlignRight)
         content_layout2.addWidget(self.lcd_current)
         content_layout2.addWidget(self.ts_label_current)
 
-        content_layout3 = self.create_page('Tagesstatistik')
+        content_layout3 = self.create_page("Tagesstatistik")
 
-        #self.lcd_min = QLCDNumber(self)
-        #self.lcd_max = QLCDNumber(self)
-        #self.lcd_avg = QLCDNumber(self)
-        #self.lcd_w24 = QLCDNumber(self)
-        #self.lcd_wtoday = QLCDNumber(self)
-        #self.label_c24 = QLabel('---,--- €')
-        #self.label_ctoday = QLabel('---,--- €')
+        # self.lcd_min = QLCDNumber(self)
+        # self.lcd_max = QLCDNumber(self)
+        # self.lcd_avg = QLCDNumber(self)
+        # self.lcd_w24 = QLCDNumber(self)
+        # self.lcd_wtoday = QLCDNumber(self)
+        # self.label_c24 = QLabel('---,--- €')
+        # self.label_ctoday = QLabel('---,--- €')
 
         p3_top_container = QWidget(self)
         p3_top_layout = QVBoxLayout(p3_top_container)
 
-        self.groupBoxMin = QGroupBox("Min")
-        verticalLayoutMin = QVBoxLayout(self.groupBoxMin)
-        self.lcdNumberMin = QLCDNumber(self.groupBoxMin)
-        verticalLayoutMin.addWidget(self.lcdNumberMin)
-        p3_top_layout.addWidget(self.groupBoxMin)
-
-        # QGroupBox für Max-Wert
-        self.groupBoxMax = QGroupBox("Max")
-        verticalLayoutMax = QVBoxLayout(self.groupBoxMax)
-        self.lcdNumberMax = QLCDNumber(self.groupBoxMax)
-        verticalLayoutMax.addWidget(self.lcdNumberMax)
-        p3_top_layout.addWidget(self.groupBoxMax)
-
-        # QGroupBox für Avg-Wert
-        self.groupBoxAvg = QGroupBox("Avg")
-        verticalLayoutAvg = QVBoxLayout(self.groupBoxAvg)
-        self.lcdNumberAvg = QLCDNumber(self.groupBoxAvg)
-        self.lcdNumberAvg.setDigitCount(5)
-        self.lcdNumberAvg.display(88888)  # Beispielwert
-        self.lcdNumberAvg.setSegmentStyle(QLCDNumber.Filled)
-        self.lcdNumberAvg.setMinimumHeight(100)
-        verticalLayoutAvg.addWidget(self.lcdNumberAvg)
-        p3_top_layout.addWidget(self.groupBoxAvg)
-
-        groupBox = QGroupBox("GroupBox", self)  # Titel der GroupBox kann angepasst werden
+        groupBox = QGroupBox(
+            "GroupBox", self
+        )  # Titel der GroupBox kann angepasst werden
         gridLayout = QGridLayout(groupBox)
 
         # Verbrauch heute
         labelToday = QLabel("Verbrauch heute")
         gridLayout.addWidget(labelToday, 0, 0)
 
-        lcdNumberToday = QLCDNumber()
-        gridLayout.addWidget(lcdNumberToday, 0, 1)
+
+        self.consumptionToday = QLabel("----")
+        self.consumptionToday.setStyleSheet(glow_style) 
+        self.consumptionToday.setFont(QFont("Arial", 20))
+        gridLayout.addWidget(self.consumptionToday, 0, 1)
 
         labelTodayUnit = QLabel("kWh")
         gridLayout.addWidget(labelTodayUnit, 0, 2)
 
-        labelTodayCost = QLabel("12,90 €")
-        labelTodayCost.setFont(QFont("Arial", 14))
-        gridLayout.addWidget(labelTodayCost, 0, 3)
+        self.labelTodayCost = QLabel("----- €")
+        self.labelTodayCost.setFont(QFont("Arial", 14))
+        gridLayout.addWidget(self.labelTodayCost, 0, 3)
 
-        # Verbrauch letzte 24 Stunden
-        label24h = QLabel("Verbrauch 24h ")
-        gridLayout.addWidget(label24h, 1, 0)
+        p3_top_layout.addWidget(groupBox)
 
-        lcdNumber24h = QLCDNumber()
-        gridLayout.addWidget(lcdNumber24h, 1, 1)
 
-        label24hUnit = QLabel("kWh")
-        gridLayout.addWidget(label24hUnit, 1, 2)
 
-        label24hCost = QLabel("13,87 €")
-        label24hCost.setFont(QFont("Arial", 12))
-        gridLayout.addWidget(label24hCost, 1, 3)
+
+        # QGroupBox für Max-Wert
+        self.groupBoxMax = QGroupBox("MAX(P) der letzten 24h")
+        verticalLayoutMax = QVBoxLayout(self.groupBoxMax)
+        self.maxW = QLabel("12345 W")
+        self.maxW.setFont(QFont("Arial", 12))
+
+        self.groupBoxMin = QGroupBox("MIN(P) der letzten 24h")
+        verticalLayoutMin = QVBoxLayout(self.groupBoxMin)
+        self.minW = QLabel("12345 W")
+        self.minW.setFont(QFont("Arial", 12))
+        verticalLayoutMin.addWidget(self.minW)
+
+        p3_top_layout.addWidget(self.groupBoxMax)
+
+        # QGroupBox für Avg-Wert
+        self.groupBoxAvg = QGroupBox("Ø(P) der letzten 24h")
+        verticalLayoutAvg = QVBoxLayout(self.groupBoxAvg)
+        self.avgW = QLabel("12345 W")
+        self.avgW.setFont(QFont("Arial", 12))
+
+        verticalLayoutAvg.addWidget(self.avgW)
+        verticalLayoutMax.addWidget(self.maxW)
+        p3_top_layout.addWidget(self.groupBoxMin)
+
+        p3_top_layout.addWidget(self.groupBoxAvg)
 
 
 
         
 
-
-
-
-
-        content_label3 = QLabel('Weitere Informationen auf Seite 3.')
+        
         content_layout3.addWidget(p3_top_container)
-        content_layout3.addWidget(groupBox)
+
+
+        content_layout4 = self.create_page("Kumulativer Zähler")
+        content_layout4.addWidget(QLabel("kWh"), alignment=Qt.AlignRight)
+        self.reset_button = QPushButton("Reset", self)
+        #self.reset_button.clicked.connect(self.reset_counter)
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        content_layout4.addWidget(line)
+        content_layout4.addWidget(QLabel("Verbrauch seit"), alignment=Qt.AlignCenter)
+        content_layout4.addWidget(QLabel("gbhjnkml"), alignment=Qt.AlignRight)
+        content_layout4.addWidget(line)
+        
+        content_layout4.addWidget(self.lcd_kulm)
+        content_layout4.addWidget(self.reset_button)
+        
         middle_layout.addWidget(self.stackedWidget)
 
         # ProgressBar
@@ -222,16 +282,16 @@ class MyApp(QWidget):
         middle_layout.addWidget(self.progress_bar)
 
         # Button links
-        self.btn_left = QPushButton('←', self)
-        self.btn_left.setFixedSize(100, 400)
+        self.btn_left = QPushButton("←", self)
+        self.btn_left.setFixedSize(100, 300)
         layout.addWidget(self.btn_left)
 
         # Fügen Sie das mittlere Container-Widget hinzu
         layout.addWidget(middle_container)
 
         # Button rechts
-        self.btn_right = QPushButton('→', self)
-        self.btn_right.setFixedSize(100, 400)
+        self.btn_right = QPushButton("→", self)
+        self.btn_right.setFixedSize(100, 300)
         layout.addWidget(self.btn_right)
 
         # Button-Funktionalitäten
@@ -253,8 +313,7 @@ class MyApp(QWidget):
         self.progress_timer.start()
 
         # Thread für Netzwerkanfragen
-        self.dataThread = DataThread(
-            "http://localhost:5000/api/energy/consumption")
+        self.dataThread = DataThread("http://localhost:5000/api/energy/consumption")
         self.dataThread.dataFetched.connect(self.update_display)
 
     def show_previous_page(self):
@@ -272,7 +331,7 @@ class MyApp(QWidget):
 
     def update_progress_bar(self):
         if self.progress_value < 100:
-            self.progress_value += 10  # Erhöht um 10% jede Sekunde
+            self.progress_value += 50  # Erhöht um 10% jede Sekunde
 
         else:
             self.progress_value = 0
@@ -284,12 +343,20 @@ class MyApp(QWidget):
         # Aktualisieren Sie hier Ihre Info-Displays basierend auf den empfangenen Daten
         # self.page1.setText(str(data))  # Beispiel zur Anzeige der Daten
         print(str(data))
-        self.lcd_zaehlerstand.display(
-            int(data["currentCounter"]["_value"]))
-        self.lcd_current.display(
-            int(data["latestValue"]["_value"]))
-        self.ts_label_current.setText(self.__convert_to_local_time(data["currentCounter"]["_time"], "Datensatz vom"))
+        self.lcd_zaehlerstand.display(int(data["currentCounter"]["_value"]))
+        self.lcd_current.display(int(data["latestValue"]["_value"]))
+        self.ts_label_current.setText(
+            self.__convert_to_local_time(
+                data["currentCounter"]["_time"], "Datensatz vom"
+            )
+        )
         print(self.__convert_to_local_time(data["currentCounter"]["_time"]))
+        today_total = (data["currentCounter"]["_value"] - data["startofdayCounter"]["_value"]) / 1000
+        self.minW.setText(f'{data["minValue"]["_value"]:.1f} W')
+        self.maxW.setText(f'{data["maxValue"]["_value"]:.1f} W')
+        self.avgW.setText(f'{data["avgValue"]["_value"]:.1f} W')
+        self.consumptionToday.setText(f'{today_total:.1f}')
+        self.labelTodayCost.setText(f'{(today_total * 0.12):.2f} €')
 
     def __convert_to_local_time(self, utc_time, prefix=None, suffix=None):
         # Angenommen, data["currentCounter"]["_time"] ist ein datetime-Objekt in UTC
@@ -297,7 +364,7 @@ class MyApp(QWidget):
 
         # Konvertierung in die Zeitzone 'Europe/Berlin'
         berlin_time = utc_time.astimezone(ZoneInfo("Europe/Berlin"))
-        berlin_time.strftime('%d.%m.%Y %H:%M:%S')
+        berlin_time.strftime("%d.%m.%Y %H:%M:%S")
 
         if prefix is not None:
             berlin_time = f"{prefix} {berlin_time}"
@@ -314,7 +381,7 @@ class MyApp(QWidget):
 
         # Überschrift
         header = QLabel(title)
-        header.setStyleSheet('font-size: 18px; font-weight: bold;')
+        header.setStyleSheet("font-size: 18px; font-weight: bold;")
         page_layout.addWidget(header)
 
         # Trennlinie
@@ -327,9 +394,8 @@ class MyApp(QWidget):
         content_layout = QVBoxLayout()
 
         # Spacer, der den Inhalt nach unten drückt
-        spacer = QSpacerItem(20, 40, QSizePolicy.Minimum,
-                             QSizePolicy.Expanding)
-        page_layout.addSpacerItem(spacer)
+        #spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        #page_layout.addSpacerItem(spacer)
 
         # Fügt das Inhalt-Layout hinzu, das sich anpassen kann, um den verbleibenden Platz zu füllen
         page_layout.addLayout(content_layout)
@@ -338,11 +404,11 @@ class MyApp(QWidget):
         return content_layout  # Gibt das Layout für Inhalte zurück
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
     app = QApplication(sys.argv)
-    app.setStyle('Fusion')
-    kiosk_mode = '--kiosk' in sys.argv
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    kiosk_mode = "--kiosk" in sys.argv
 
     ex = MyApp(kiosk_mode=kiosk_mode)
     ex.show()
